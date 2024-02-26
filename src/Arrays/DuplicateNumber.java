fa144ff41d4318b97b3c3e904ea660edec62f54b
MdAyazAnwar <anwer.khan14@gmail.com>
2024-02-26 11:27:47 +0530

    Code for duplicate number and find missing number in array

---
+++ src/Arrays/DuplicateNumber.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Arrays;
+
+/**
+ * Write a function which takes integer array as a parameter and returns a new integer array with unique elements.
+ * (remove duplicates)
+ */
+public class DuplicateNumber {
+
+    public static void main(String[] args) {
+        int[] arr =  {1, 1, 2, 2, 3, 3, 4, 5};
+        int[] uniqueArray =  removeDuplicate(arr);
+        for( int number :  uniqueArray) {
+            System.out.print(number + " " );
+        }
+    }
+
+    public static int[] removeDuplicate(int[] arr) {
+        int n  = arr.length;
+        int[] uniqueArray = new int[n];
+        int index  = 0;
+        for( int i  =  0; i < n; i++) {
+            boolean isDuplicate = false;
+            for( int j = i + 1; j < n; j++) {
+                if(arr[i] ==  arr[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(!isDuplicate) {
+                uniqueArray[index++] =  arr[i];
+            }
+        }
+        return Arrays.copyOf(uniqueArray, index);
+    }
+}
---
+++ src/Arrays/FindMissingNumber.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+
+/**
+ * Write Java function called findMissingNumberInArray that takes an integer array
+ * containing n-1 unique elements from a range of 1 to n, with one missing number,
+ * and returns the missing number.
+ */
+public class FindMissingNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6};
+        System.out.println("missing number in given array: "+  findMissingNumberInArray(arr));
+    }
+
+    public static int findMissingNumberInArray(int[] arr) {
+        int n  = arr.length + 1;
+        int expectedSum =  n * ( n + 1 ) / 2;
+        int actual = 0;
+        for (int number : arr) {
+             actual += number;
+        }
+        return  expectedSum - actual;
+    }
+}
