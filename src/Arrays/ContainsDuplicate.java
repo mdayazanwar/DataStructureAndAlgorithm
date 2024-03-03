bcce61563e405348b2136620cb89b4c55f0872ce
MdAyazAnwar <anwer.khan14@gmail.com>
2024-03-03 18:10:09 +0530

    Contains duplicates and permutation impl

---
+++ src/Arrays/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+/**
+ * Write a program to check if an array is unique or not
+ */
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2, 3, 4, 5, 6, 7};
+        boolean result =  isUnique(arr);
+        System.out.println(result);
+    }
+    public static boolean isUnique(int[] arr) {
+        for ( int i  = 0 ; i< arr.length; i++) {
+            for( int j = i+1; j< arr.length; j++) {
+                if(arr[i] == arr[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/Arrays/Permutation.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {5, 4, 1, 2, 3};
+        boolean result =  permutation(arr1, arr2 );
+        System.out.println(result);
+    }
+
+    public static boolean permutation(int[] array1, int[] array2) {
+        boolean result= false;
+        for (int j : array1) {
+            result = false;
+            for (int k : array2) {
+                if (j == k) {
+                    result = true;
+                    break;
+                }
+            }
+            if (!result) {
+                return result;
+            }
+        }
+        return result;
+    }
+}
