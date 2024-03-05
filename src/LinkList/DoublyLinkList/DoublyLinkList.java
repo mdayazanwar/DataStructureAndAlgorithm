9363c9b4bd9a21d5bfb681ea649a2d68ef656bbe
MdAyazAnwar <anwer.khan14@gmail.com>
2024-03-05 09:53:01 +0530

    Doubly Linked List Complete impl

--- src/LinkList/DoublyLinkList/DoublyLinkList.java
+++ src/LinkList/DoublyLinkList/DoublyLinkList.java
@@ -67,4 +67,59 @@ public class DoublyLinkList {
             System.out.println(temoNode.value);
         }
     }
+
+    public void search(int value) {
+         Node temp = head;
+         for( int i = 0 ; i< size ; i++) {
+            if( temp.value == value) {
+                System.out.println("Element " + value+" found at position " + i );
+                return;
+            }
+            else {
+                temp = temp.next;
+            }
+        }
+         System.out.println("Element not found in the list");
+    }
+
+    public void delete(int position) {
+        if (head == null) {
+            System.out.println("The DLL does not exist!");
+            return;
+        }
+
+        if (position < 0 || position >= size) {
+            System.out.println("Invalid position");
+            return;
+        }
+
+        if (size == 1) {
+            // Deleting the only node in the list
+            head = null;
+            tail = null;
+        } else if (position == 0) {
+            // Deleting the first node
+            head = head.next;
+            head.prev = null;
+        } else if (position == size - 1) {
+            // Deleting the last node
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            // Deleting a node in the middle
+            Node tempNode = head;
+            for (int i = 0; i < position; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.prev.next = tempNode.next;
+            tempNode.next.prev = tempNode.prev;
+        }
+
+        size--;
+    }
+    public void deleteEntireList() {
+         head  = null;
+         tail = null;
+         System.out.println("List has been deleted successfully");
+    }
 }
